eb242ffeba45ef64b923eddd33c5d0ffebdf1ac5
MD SAYDUR RAHMAN <93133412+engrsayed90@users.noreply.github.com>
2023-04-24 06:02:44 +0900

    Update TopBar.jsx

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -6,7 +6,7 @@ import "./topbar.css";
 
 export default function TopBar() {
   const { user, dispatch } = useContext(Context);
-  const PF = "http://localhost:5000/images/"
+  const PF = "https://react-api-vdto.onrender.com/images/"
 
   const handleLogout = () => {
     dispatch({ type: "LOGOUT" });
